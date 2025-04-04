b76c34b6024fe53eff1618cdc62dc256bed3ae9b
RicardoMota22 <ricardomota.artist@gmail.com>
2025-04-04 10:27:43 +0100

    6th exercise, Winrate

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        public float highScore {get; private set;}
+        public int playedGames {get; private set;}
+
+        public int wonGames {get; private set;}
+
+        public float HighScore
+        {
+            get
+            { 
+                return highScore; 
+            }
+            set
+            {
+                if (value > highScore)
+                {
+                    highScore = value;
+                }
+            }
+        }
+
+        public string Name {get;}
+
+        public float WinRate
+        {
+            get 
+            {
+                return (float) wonGames / playedGames; 
+            }
+            set
+            {
+                if (playedGames == 0)
+                WinRate = 0;
+            }
+        }
+
+
+    }
+}
